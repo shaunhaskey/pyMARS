90a4a59dfd07f7ac51268c0a08b1ca8753bc9686
Shaun Haskey <haskeysr@benten.gat.com>
2012-11-05 22:24:50 -0800

    not much...

--- python/MARS_post_processing/python_plotk.py
+++ python/MARS_post_processing/python_plotk.py
@@ -32,13 +32,14 @@ for i in range(0,m.shape[0]):
     mode_amps[i,:] = (np.array(output_dict['REAL'][m[i]])+1j*(np.array(output_dict['IMAG'][m[i]])))
 fig, ax = pt.subplots()
 ax.imshow(np.abs(mode_amps))
+mode_amps *= 2.*np.pi
 
 fig.canvas.draw(); fig.show()
 s = np.loadtxt('PROFEQ.OUT')[:,0]
 print s.shape
-
+m *= -1
 fig, ax = pt.subplots()
-color_plot = ax.pcolor(m, np.sqrt(s[1:-1]),np.real(mode_amps).transpose(),cmap='hot')
+color_plot = ax.pcolor(m, np.sqrt(s[1:-1]),np.abs(mode_amps).transpose(),cmap='hot')
 pt.colorbar(color_plot, ax = ax)
 fig.canvas.draw(); fig.show()
 
@@ -49,7 +50,7 @@ tmp_file = h5py.File('spectral_info.h5')
 stored_data = tmp_file.get('1')
 zdat = stored_data[0][0]; xdat = stored_data[0][1]; ydat = stored_data[0][2]
 image1 = ax_tmp[0].pcolor(xdat,ydat,zdat,cmap = 'hot')
-color_plot = ax_tmp[1].pcolor(m, (s[1:-1]),np.real(mode_amps).transpose(),cmap='hot')
+color_plot = ax_tmp[1].pcolor(m, (s[1:-1]),np.abs(mode_amps).transpose(),cmap='hot')
 ax_tmp[0].set_title('Matt routine')
 ax_tmp[1].set_title('SH routine')
 pt.colorbar(color_plot, ax = ax_tmp[1])
