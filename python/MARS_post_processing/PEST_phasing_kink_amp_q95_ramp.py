18407fc6f343da8b5932f45bbe8d6fa8d8d2a209
Shaun Haskey <shaunhaskey@gmail.com>
2012-11-21 19:24:24 +1100

    strange_change

--- python/MARS_post_processing/PEST_phasing_kink_amp_q95_ramp.py
+++ python/MARS_post_processing/PEST_phasing_kink_amp_q95_ramp.py
@@ -349,6 +349,7 @@ if plot_PEST_pics:
         #color_plot.set_clim([0,3])
         ax.set_title(suptitle)
         cbar = pt.colorbar(color_plot, ax = ax)
+        ax.plot([-29,29],[psi,psi], 'b--')
         ax.set_xlabel('m')
         ax.set_ylabel(r'$\sqrt{\psi_N}$', fontsize = 14)
         cbar.ax.set_ylabel(r'$\delta B_r$ (G/kA)')
