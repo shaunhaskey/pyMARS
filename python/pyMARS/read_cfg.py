170a05b3c0514ea7756dd851bb7cee37d8386c41
Shaun Haskey <shaunhaskey@gmail.com>
2014-03-12 11:08:38 +1100

    small mods to pymars rm files

--- python/pyMARS/read_cfg.py
+++ python/pyMARS/read_cfg.py
@@ -8,6 +8,7 @@ import numpy as num
 import ConfigParser
 import numpy as num
 import numpy as np
+import shutil
 config_filename = sys.argv[1]
 parser = ConfigParser.SafeConfigParser()
 parser.optionxform=str
@@ -304,17 +305,19 @@ if start_from_step == 1:
             
             #rotation_filename = 'dpr' + gfile_list[i].split('.')[0][1:] + '.' + str(time_tmp) + '_Er_RBpol.dat'
 
-
-            os.system('cp ' + project_dict['details']['profile_master'] + '/' + density_filename + ' ' + project_dict['details']['multiple_efit'][-1])
-            os.system('cp ' + project_dict['details']['profile_master'] + '/' + rotation_filename + ' ' + project_dict['details']['multiple_efit'][-1])
-            os.system('cp ' + project_dict['details']['profile_master'] + '/' + ti_filename + ' ' + project_dict['details']['multiple_efit'][-1])
-            os.system('cp ' + project_dict['details']['profile_master'] + '/' + te_filename + ' ' + project_dict['details']['multiple_efit'][-1])
-            os.system('cp ' + project_dict['details']['profile_master'] + '/' + density_filename + ' ' + project_dict['details']['multiple_efit'][-1] +'/PROFDEN')
-            os.system('cp ' + project_dict['details']['profile_master'] + '/' + rotation_filename + ' ' + project_dict['details']['multiple_efit'][-1] + '/PROFROT')
-            inc_ti_te = True
-            if inc_ti_te:
-                os.system('cp ' + project_dict['details']['profile_master'] + '/' + ti_filename + ' ' + project_dict['details']['multiple_efit'][-1] + '/PROFTI')
-                os.system('cp ' + project_dict['details']['profile_master'] + '/' + te_filename + ' ' + project_dict['details']['multiple_efit'][-1] + '/PROFTE')
+            for tmp_file, mars_name in zip([density_filename, rotation_filename, ti_filename, te_filename],['PROFDEN', 'PROFROT', 'PROFTI', 'PROFTE']):
+                shutils.copy('{}/{}'.format(project_dict['details']['profile_master'], tmp_file), project_dict['details']['multiple_efit'][-1])
+                shutils.copy('{}/{}'.format(project_dict['details']['profile_master'], tmp_file), '{}/{}'.format(project_dict['details']['multiple_efit'][-1], mars_name))
+            #os.system('cp ' + project_dict['details']['profile_master'] + '/' + density_filename + ' ' + project_dict['details']['multiple_efit'][-1])
+            #os.system('cp ' + project_dict['details']['profile_master'] + '/' + rotation_filename + ' ' + project_dict['details']['multiple_efit'][-1])
+            #os.system('cp ' + project_dict['details']['profile_master'] + '/' + ti_filename + ' ' + project_dict['details']['multiple_efit'][-1])
+            #os.system('cp ' + project_dict['details']['profile_master'] + '/' + te_filename + ' ' + project_dict['details']['multiple_efit'][-1])
+            #os.system('cp ' + project_dict['details']['profile_master'] + '/' + density_filename + ' ' + project_dict['details']['multiple_efit'][-1] +'/PROFDEN')
+            #os.system('cp ' + project_dict['details']['profile_master'] + '/' + rotation_filename + ' ' + project_dict['details']['multiple_efit'][-1] + '/PROFROT')
+            #inc_ti_te = True
+            #if inc_ti_te:
+            #    os.system('cp ' + project_dict['details']['profile_master'] + '/' + ti_filename + ' ' + project_dict['details']['multiple_efit'][-1] + '/PROFTI')
+            #    os.system('cp ' + project_dict['details']['profile_master'] + '/' + te_filename + ' ' + project_dict['details']['multiple_efit'][-1] + '/PROFTE')
             shot_number = int(gfile_list[i][1:7])
 
         project_dict['details']['shot'] = shot_number
