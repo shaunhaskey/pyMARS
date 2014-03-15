5f5e16ea65c62d515dfe1dc5b766248458184dae
Shaun Haskey DIII-D <shaunhaskey@gmail.com>
2014-03-15 00:52:41 -0700

    including the cluster based image scans

---
+++ python/MARS_post_processing/plot_scan_PEST_pics_qsub_exec.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env Python
+'''
+SH : Nov 21 2012
+This is useful for creating PEST plot images
+It can make an animation and introduce different phasings
+'''
+import  results_class
+from RZfuncs import I0EXP_calc
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as pt
+import copy
+import cPickle as pickle
+import PythonMARS_funcs as pyMARS
+import multiprocessing
+import itertools, os, sys
+import scan_pics_func
+
+keys = [int(sys.argv[i]) for i in range(1,len(sys.argv))]
+
+N = 6; n = 3
+I = np.array([1.,-1.,0.,1,-1.,0.])
+I = np.array([1.,-1.,1.,-1,-1.,-1.])
+I0EXP = results_class.I0EXP_calc_real(n,I)
+facn = 1.0 #WHAT IS THIS WEIRD CORRECTION FACTOR?
+file_name = '/u/haskeysr/mars/shot_142614_rote_res_scan_20x20_kpar1_low_rote/shot_142614_rote_res_scan_20x20_kpar1_low_rote_post_processing_PEST.pickle'
+with file(file_name, 'r') as file_handle: scan_data = pickle.load(file_handle)
+
+clim = [0,3.0]
+base_dir = '/u/haskeysr/tmp_ims/'
+
+subplot_plot = 'total'
+inc_contours = False
+valid_sim_list = []
+im_name_list = []
+title_list = []
+for i in keys: valid_sim_list.append(copy.deepcopy(scan_data['sims'][i]))
+for i in keys:
+    #im_name_list.append('{}/ROTE_{:010d}_ETA_{:010d}_{}.png'.format(base_dir, int(scan_data['sims'][i]['MARS_settings']['<<ROTE>>']*10**10), int(scan_data['sims'][i]['MARS_settings']['<<ETA>>']*10**10), subplot_plot))
+    im_name_list.append('{}/ROTE_{:010d}_ETA_{:010d}'.format(base_dir, int(scan_data['sims'][i]['MARS_settings']['<<ROTE>>']*10**10), int(scan_data['sims'][i]['MARS_settings']['<<ETA>>']*10**10)))
+    title_list.append('ROTE_{:.3e} ETA {:.3e}'.format(scan_data['sims'][i]['MARS_settings']['<<ROTE>>'], scan_data['sims'][i]['MARS_settings']['<<ETA>>']))
+print im_name_list
+
+input_data = zip(valid_sim_list, im_name_list, itertools.repeat(I0EXP), itertools.repeat(facn), itertools.repeat(subplot_plot), itertools.repeat(n), itertools.repeat(inc_contours), itertools.repeat(clim), title_list)
+
+map(scan_pics_func.plot_scan, input_data)
+
+#pool = multiprocessing.Pool(10, maxtasksperchild = 1)
+#pool.map(scan_pics_func.plot_scan, input_data)
+
---
+++ python/MARS_post_processing/scan_pics_func.py
@@ -0,0 +1,82 @@
+import  results_class
+from RZfuncs import I0EXP_calc
+import numpy as np
+import matplotlib.pyplot as pt
+import copy
+import cPickle as pickle
+import PythonMARS_funcs as pyMARS
+import multiprocessing
+import itertools, os
+def plot_scan(args):
+    #print 'hello'                                                                                                           
+    print 'hello plots', os.getpid(), os.getcwd()
+    valid_sim, im_name, I0EXP, facn, subplot_plot, n, inc_contours, clim, fig_title = copy.deepcopy(args)
+    combs = itertools.product(['upper','lower'],['plasma','vacuum'])
+    simuls = {}
+    for loc, typ in combs:
+        tmp = results_class.data(valid_sim['dir_dict']['mars_{}_{}_dir'.format(loc, typ)], I0EXP = I0EXP, getpest = True)
+        print '---',  os.getcwd(), os.getcwd(), np.sum(np.abs((tmp.Bn))), np.sum(np.abs((tmp.BnPEST)))
+        #print '---',  os.getpid(), os.getcwd(), np.sum(np.abs((tmp.Bn)))
+        #tmp.get_PEST(facn=facn)
+        simuls['{}_{}'.format(loc,typ)] = copy.deepcopy(tmp)
+    #for i in simuls.keys():
+    #    print '!!',os.getpid(), i, 
+    #for i in simuls.keys(): simuls[i].get_PEST(facn = facn)
+    #for i in simuls.keys():
+    #    print '!!', os.getpid(), i, np.sum(np.abs((simuls[i].BnPEST)))
+    phasings = [0,90,180,270]
+    fig2,ax2 = pt.subplots(ncols = 3, sharex =True, sharey = True)
+    for ind, subplot_plot in enumerate(['vac','plasma','total']):
+        fig,ax = pt.subplots(nrows = 2, ncols = 2, sharex =True, sharey = True)
+        ax[0,0].set_ylabel(r'$\sqrt{\psi_N}$', fontsize = 14)
+        ax[1,0].set_ylabel(r'$\sqrt{\psi_N}$', fontsize = 14)
+        ax[1,0].set_xlabel('m')
+        ax[1,1].set_xlabel('m')
+        ax = ax.flatten()
+        color_plots = []
+        for i, phasing in enumerate(phasings):
+            print phasing
+            combined = copy.deepcopy(simuls['upper_plasma'])
+            #Combine the upper and lower data with the appropriate phasing
+            R_t, Z_t, B1_t, B2_t, B3_t, Bn_t, BMn_t, BnPEST_t = results_class.combine_data(simuls['upper_plasma'], simuls['lower_plasma'], phasing)
+            R_v, Z_v, B1_v, B2_v, B3_v, Bn_v, BMn_v, BnPEST_v = results_class.combine_data(simuls['upper_vacuum'], simuls['lower_vacuum'], phasing)
+            if i==0: print 'totals are ', os.getpid(), fig_title, np.sum(np.abs(BnPEST_t)), np.sum(np.abs(BnPEST_v))
+            #Choose which plot to create
+            cmap = 'spectral'
+            if subplot_plot=='total':
+                combined.BnPEST = +BnPEST_t
+                combined.BnPEST[np.isnan(combined.BnPEST)] = 0
+                print 'asdf total ', phasing, np.sum(np.abs(combined.BnPEST))
+                color_plots.append(combined.plot_BnPEST(ax[i], n=n, inc_contours = inc_contours, cmap = cmap))
+                #color_plots.append(combined.plot_BnPEST(ax[i], n=n, inc_contours = 1, increase_grid_BnPEST=1, gauss_filter = [0,0.05]))
+            elif subplot_plot=='vac':
+                combined.BnPEST = +BnPEST_v
+                combined.BnPEST[np.isnan(combined.BnPEST)] = 0
+                print 'asdf vac ', phasing, np.sum(np.abs(combined.BnPEST))
+                color_plots.append(combined.plot_BnPEST(ax[i], n=n, inc_contours = inc_contours, cmap = cmap))
+            elif subplot_plot=='plasma':
+                combined.BnPEST = BnPEST_t - BnPEST_v
+                combined.BnPEST[np.isnan(combined.BnPEST)] = 0
+                print 'asdf plasma ', phasing, np.sum(np.abs(combined.BnPEST)), np.sum(np.abs(BnPEST_v)), np.sum(np.abs(BnPEST_t))
+                color_plots.append(combined.plot_BnPEST(ax[i], n=n, inc_contours = inc_contours, cmap = cmap))
+            if phasing==0:
+                tmp_clr = combined.plot_BnPEST(ax2[ind], n=n, inc_contours = inc_contours, cmap = cmap)
+                ax2[ind].set_title('{}'.format(np.sum(np.abs(combined.BnPEST))))
+                tmp_clr.set_clim(clim)
+            ax[i].set_title(r'$\Delta \phi_{ul} = %d^o$'%(phasing),fontsize = 18)
+            color_plots[-1].set_clim(clim)
+        ax[0].set_xlim([-25,25])
+        ax[0].set_ylim([0.4,1])
+        fig.suptitle('{} {}'.format(fig_title,subplot_plot))
+        #pt.colorbar(ax[-1],ax = ax.flatten().tolist())
+        fig.savefig('{}_{}.png'.format(im_name, subplot_plot))
+        fig.clf()
+        #fig.close()
+
+    ax2[0].set_xlim([-25,25])
+    ax2[0].set_ylim([0.4,1])
+    fig2.suptitle(fig_title)
+    #pt.colorbar(ax[-1],ax = ax.flatten().tolist())
+    fig2.savefig('{}_all.png'.format(im_name))
+    fig2.clf()
+    #fig.close()
